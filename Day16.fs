2b4e4e5ae7443d1b799be266217e89f897bfc7ce
Neerav Kothari <neerav.kothari@gmail.com>
2021-03-31 10:27:30 -0400

    Day16 updates

--- Day16.fs
+++ Day16.fs
@@ -120,3 +120,135 @@ module Part1 =
         let _, notInRange = partitionNumbers fieldRanges ticketValues
 
         List.sum notInRange
+
+module Part2 =
+
+    open Part1
+    
+    let isTicketInvalid fieldRanges ticket =
+        let isInvalid = isInAnyRange fieldRanges >> not
+        ticket.Values
+        |> List.exists (isInvalid)
+
+    let isOutOfRange value range1 range2 =
+        let min1,max1 = range1
+        let min2,max2 = range2
+        let x = ((value < min1) || (value > max1)) && ((value < min2) || (value > max2))
+        x
+
+    let isColumnInRanges colValues range1 range2 =
+        // check if min and max are both in either range
+        // if so, thenthe whole column is in range
+        let sortedValues = Array.sort colValues
+        let rec recIsColumnInRanges (sv:int array) range1 range2 =
+            let min, max = sv.[0], Array.last sv
+            let min1,max1 = range1
+            let min2,max2 = range2
+            if (((min1 <= min) && (max <= max1)) || ((min2 <= min) && (max <= max2))) then
+                true
+            else
+                if ((isOutOfRange min range1 range2) || (isOutOfRange max range1 range2)) then
+                    false
+                else
+                    // if there are only 2 value in the array and you reach here, that means
+                    // both values are in some range. we are done.
+                    if (Array.length sv = 2) then
+                        true
+                    else
+                        // need to check if there are any values that fall outside the 2 ranges
+                        // get the mid point. if it falls outside the ranges, we are done as
+                        // it falls outside the ranges. if it falls inside one of the ranges, then
+                        // we need to check the other half to see if it falls outside the range.
+                        let midPoint = int (floor (((float (Array.length sortedValues)) - (float 0))/ float 2))
+                        let midValue = sortedValues.[midPoint]
+                        if (isOutOfRange midValue range1 range2) then
+                            false
+                        else
+                            // check which half is in range
+                            if ((min1 <= midValue) && (midValue <=max1)) then
+                                // check second half of the array
+                                let toCheck = snd (Array.splitAt (midPoint + 1) sv)
+                                recIsColumnInRanges toCheck range1 range2
+                            else
+                                // check first half of range
+                                let toCheck = fst (Array.splitAt (midPoint + 1) sv)
+                                recIsColumnInRanges toCheck range1 range2
+
+        recIsColumnInRanges sortedValues range1 range2
+        
+    let Solution file =
+
+        let _,myTicket,fieldInformation, nearbyTickets = 
+            File.ReadLines file
+            |> Seq.fold (fun (prevWasMyTkHdr,myTkt, fieldInfos, ticketInfos) line ->
+                let d = Data.parse line
+                match d with
+                | FieldInfo fi ->
+                    // add to field info list
+                    false,myTkt, fieldInfos@[fi], ticketInfos
+                | MyTicketHeader ->
+                    true,myTkt, fieldInfos, ticketInfos
+                | NearbyTicketHeader ->
+                    false,myTkt, fieldInfos, ticketInfos
+                | Ticket tkt ->
+                    if (prevWasMyTkHdr) then
+                        false,tkt, fieldInfos, ticketInfos
+                    else
+                        false,myTkt, fieldInfos, ticketInfos@[tkt]
+                | NotUsed ->
+                    false,myTkt, fieldInfos, ticketInfos
+            ) (false, Ticket.empty, List.empty, List.empty)
+
+        // flatten out all the field value range info into 1 list
+        let fieldRanges =
+            fieldInformation
+            |> List.map (fun fi -> fi.Name,fi.Ranges)
+            |> Map.ofList
+            
+        let allFieldRanges =
+            Array.collect snd (Map.toArray fieldRanges)
+
+        // get only valid tickets
+        let validTickets =
+            nearbyTickets
+            |> List.filter (isTicketInvalid allFieldRanges >> not)
+
+        // bucket all values for the same column together
+        // Initialize the buckets
+        let emptyBuckets =
+            [0..(List.length myTicket.Values - 1)]
+            |> List.fold (fun map col ->
+                Map.add col List.empty map
+            ) Map.empty
+        // Add values into each bucket
+        let columnBuckets =
+            validTickets
+            |> List.fold (fun bucket tkt ->
+                tkt.Values
+                |> List.fold (fun (col, map) v ->
+                    let list = Map.find col map
+                    col + 1, Map.add col (list@[v]) map
+                ) (0,bucket)
+                |> snd
+            ) emptyBuckets
+            |> Map.map (fun col bucket -> List.toArray bucket)
+
+        let columnFieldMapping =
+            columnBuckets
+            |> Map.map (fun col bucket ->
+                // check if the column fits into any of the field ramges
+                let x = 
+                    fieldInformation
+                    |> List.fold (fun state fi ->
+                        if (Option.isNone state) then
+                            if (isColumnInRanges bucket fi.Ranges.[0] fi.Ranges.[1]) then
+                                Some(fi)
+                            else None
+                        else
+                            state
+                    ) None
+                x
+            )
+        
+        printfn "%A" columnFieldMapping
+        0
--- Program.fs
+++ Program.fs
@@ -318,7 +318,10 @@ let main argv =
     //printfn "Day15/Part 2 %A " (Day15.Part2.Solution "Day15Part1.txt")
 
 
-    // Day 15
+    // Day 16
     // Part 1
     printfn "Day16/Part 1 %A " (Day16.Part1.Solution "Day16Part1.txt")
+    // Part 2
+    printfn "Day16/Part 2 %A " (Day16.Part2.Solution "Day16Part1.txt")
+
     0 // return an integer exit code
